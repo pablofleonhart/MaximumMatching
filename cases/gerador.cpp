bb915e54d421ed8f4e811fc4c619197410d043e4
pablofleonhart <pablo.fl11@gmail.com>
2017-06-06 09:29:56 -0300

    Ajustando gerador de casos para criar 'm' arestas

--- cases/gerador.cpp
+++ cases/gerador.cpp
@@ -1,6 +1,10 @@
 #include <fstream>
 #include <iostream>
 #include <cassert>
+#include <vector>
+#include <algorithm>
+#include <cctype>
+#include <set>
 using namespace std;
  
 #include <boost/graph/adjacency_list.hpp>
@@ -12,8 +16,9 @@ typedef adjacency_list_traits<vecS,vecS,undirectedS>::vertex_descriptor Node;
 typedef adjacency_list_traits<vecS,vecS,undirectedS>::edge_descriptor Edge;
  
 // information stored in vertices
-struct VertexInformation {
-  Node mate; // partner or graph_traits<Graph>::null_vertex()
+struct VertexInformation
+{
+	Node mate; // partner or graph_traits<Graph>::null_vertex()
 };
 // information stored in edges
 struct EdgeInformation {};
@@ -21,75 +26,74 @@ struct EdgeInformation {};
 // graph is an adjacency list represented by vectors
 typedef adjacency_list<vecS,vecS,undirectedS,VertexInformation,EdgeInformation> Graph;
  
-int main(int argc, char *argv[]) {
-  assert(argc == 4);
-  unsigned n = atoi(argv[1]);
-  unsigned m = atof(argv[2]);
-  char *filename = argv[3];
- 
-  srand48(time(0));
- 
-  // (1) generate random bi-partite graph
-  Graph g;
- 
-  for(unsigned i=0; i<2*n; i++)
-    add_vertex(g);
- 
-  /*for(unsigned i=0; i<n; i++)
-    for(unsigned j=n; j<2*n; j++)
-      if (drand48() < p) {
-        Edge e = add_edge(i,j,g).first;
-      }*/
+int main( int argc, char *argv[] )
+{
+	assert( argc == 4 );
+	unsigned n = atoi( argv[1] );
+	unsigned m = atof( argv[2] );
+	char *filename = argv[3];
+	vector<pair<unsigned, unsigned> > adj;
 
-  for ( unsigned i = 0; i < m; i++ )
-  {
-    unsigned src = lrand48()%num_vertices(g);
-    unsigned dst = lrand48()%num_vertices(g);
+	srand48( time( 0 ) );
+	Graph g;
 
-    bool invalid = true;
-    while ( invalid || src == dst )
-    {
-      invalid = false;
-      graph_traits<Graph>::edge_iterator eb, ee;
-      for ( tie(eb, ee)=edges(g); eb != ee; eb++)
-      {
-        if ( source(*eb,g)+1 == src && target(*eb, g)+1 == dst )
-        {
-          invalid = true;
-        }
-      }
+	for( unsigned i = 0; i < 2*n; i++ )
+	{
+		add_vertex(g);
+	}
 
-      if ( invalid )
-      {
-        src = lrand48()%num_vertices(g);
-        dst = lrand48()%num_vertices(g);
-      }      
-    }
+	vector<pair<unsigned, unsigned> >::iterator it;
 
-    add_edge( src, dst, g ).first;
-  }
+	for ( unsigned i = 0; i < m; i++ )
+	{
+		unsigned src = lrand48()%num_vertices( g );
+		unsigned dst = lrand48()%num_vertices( g );
 
-  graph_traits<Graph>::edge_iterator eb, ee;
-  for ( tie(eb, ee)=edges(g); eb != ee; eb++)
-    cout << "e " << source(*eb,g)+1 << " " << target(*eb, g)+1 << endl;
- 
-  // (2) get maximum matching
-  edmonds_maximum_cardinality_matching(g, get(&VertexInformation::mate,g));
-  unsigned card = 0;
-  graph_traits<Graph>::vertex_iterator vb, ve;
-  for ( tie(vb, ve)=vertices(g); vb != ve; vb++)
-    if (g[*vb].mate != graph_traits<Graph>::null_vertex())
-      card++;
-  cout << "The cardinality of a maximum matching is " << card/2 << "." << endl;
- 
-  ofstream file;
-  file.open( filename );
-  // (3) print out in DIMACS format
-  file << "c Bi-partite graph" << endl << endl;
-  file << "p edge " << num_vertices(g) << " " << num_edges(g) << endl;
-  //graph_traits<Graph>::edge_iterator eb, ee;
-  for ( tie(eb, ee)=edges(g); eb != ee; eb++)
-    file << "e " << source(*eb,g)+1 << " " << target(*eb, g)+1 << endl;
+		bool invalid = true;
+		while ( invalid || src == dst || src >= ( num_vertices( g )/2 ) || dst < ( num_vertices( g )/2 ) )
+		{
+			src = lrand48()%num_vertices( g );
+		    dst = lrand48()%num_vertices( g );
+		    invalid = false;
+		  	it = find( adj.begin(), adj.end(), make_pair( src, dst ) );
 
-  file.close();
+		  	if ( it != adj.end() )
+			{
+				invalid = true;
+			}
+		}
+
+		cout << i+1 << endl;
+		add_edge( src, dst, g ).first;
+		adj.push_back( make_pair( src, dst ) );
+	}
+
+	// (2) get maximum matching
+	edmonds_maximum_cardinality_matching( g, get( &VertexInformation::mate, g ) );
+	unsigned card = 0;
+	graph_traits<Graph>::vertex_iterator vb, ve;
+
+	for ( tie( vb, ve )=vertices( g ); vb != ve; vb++ )
+	{
+		if ( g[*vb].mate != graph_traits<Graph>::null_vertex() )
+	  	{
+	  		card++;
+	  	}
+	}
+
+	cout << "The cardinality of a maximum matching is " << card/2 << "." << endl;
+
+	ofstream file;
+	file.open( filename );
+	// (3) print out in DIMACS format
+	file << "c Bi-partite graph" << endl << endl;
+	file << "p edge " << num_vertices( g ) << " " << num_edges( g ) << endl;
+	
+	graph_traits<Graph>::edge_iterator eb, ee;
+	for ( tie( eb, ee ) = edges( g ); eb != ee; eb++ )
+	{
+		file << "e " << source( *eb, g ) + 1 << " " << target( *eb, g ) + 1 << endl;
+	}
+
+	file.close();
 }
---
+++ cases/gerador.py
@@ -0,0 +1,7 @@
+import sys
+
+vertexes = sys.argv[1]
+edges = sys.argv[2]
+filename = sys.argv[3]
+
+print vertexes, edges, filename
